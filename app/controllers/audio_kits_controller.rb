345343f40accef6a55fe47287b9fbeb5d4db808d
Rin <aaronopsahl@gmail.com>
2016-03-27 16:11:36 -0500

    Connect audio kit links

--- app/controllers/audio_kits_controller.rb
+++ app/controllers/audio_kits_controller.rb
@@ -9,13 +9,17 @@ class AudioKitsController < ApplicationController
 	end
 
 	def show
-		# @audio_kit = AudioKit.find(params[:id])
-		@audio_kit = AudioKit.find(1)
-
+		@audio_kit = AudioKit.find(params[:id])
 	end
 
 	def create
+		@audio_kit = AudioKit.new(audio_kit_params)
+	end
 
+	private
+
+	def audio_kit_params
+		params.require(:audio_kit).permit(:title, :creator_id)
 	end
 
 end
