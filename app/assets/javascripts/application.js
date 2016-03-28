8c660aa6668b52fc99533ebc1b4e29868d49cee5
Rin <aaronopsahl@gmail.com>
2016-03-27 19:08:01 -0500

    Add style to Audio Kit page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,8 +30,8 @@ $(document).ready(function(){
 		var paper = Raphael(this, 200, 100);
 		var kitPad = paper.path("M10,10L10,90L90,90L90,10Z");
 
-		kitPad.attr({fill: getRandomColor()});
-		kitPad.animate({fill: "black"}, 1000, '<');
+		kitPad.attr({fill: "black"});
+		kitPad.animate({fill: getRandomColor()}, 1500, 'bounce');
 
 		kitPad.node.onclick = function(){
 			kitPad.animate({transform: "r" + 360}, 90);
